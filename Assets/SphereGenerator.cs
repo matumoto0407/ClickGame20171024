c045b3392f9b7f717a7fb3ce98565d88f7d8a357
Ryo Matumoto <nkc.ryo.matumoto.2017@gmail.com>
2017-10-26 11:15:21 +0900

    透明ボール

---
+++ Assets/SphereGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereGenerator : MonoBehaviour
+{
+    public GameObject SpherePrefab;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Sphere =
+                Instantiate(SpherePrefab) as GameObject;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            Sphere.GetComponent<SphereController>().Shoot(
+                worldDir.normalized * 2000);
+        }
+    }
+}
---
+++ Assets/SphereScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereScript : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+}
